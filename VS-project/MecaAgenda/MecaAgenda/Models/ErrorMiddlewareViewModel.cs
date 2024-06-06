468fd559cd26e3bdecc1886b6f9639b19736a130
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-06-06 12:24:41 -0600

    Add Error Middleware

---
+++ VS-project/MecaAgenda/MecaAgenda/Models/ErrorMiddlewareViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MecaAgenda.Web.Models
+{
+    public class ErrorMiddlewareViewModel
+    {
+        public String Path { get; set; } = default!;
+        public List<String> ListMessages { get; set; } = default!;
+        public String IdEvent { get; set; } = default!;
+    }
+}
