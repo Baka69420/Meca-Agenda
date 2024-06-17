38a4443c0d8c419a937c9bb0a8fcf7666b6e686e
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-06-17 17:14:13 -0600

    Add Bill List and Bill Details

---
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/BillController.cs
@@ -0,0 +1,48 @@
+﻿using MecaAgenda.Application.Services.Implementations;
+using MecaAgenda.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MecaAgenda.Web.Controllers
+{
+    public class BillController : Controller
+    {
+        private readonly IServiceBill _serviceBill;
+
+        public BillController(IServiceBill serviceBill)
+        {
+            _serviceBill = serviceBill;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var collection = await _serviceBill.ListAsync();
+            return View(collection);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                var @object = await _serviceBill.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Bill does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
