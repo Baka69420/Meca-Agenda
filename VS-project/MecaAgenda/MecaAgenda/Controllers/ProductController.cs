66aed1d9b1fc93ed4cbf552950a82ab7bde14c70
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-06-17 15:22:21 -0600

    Add Product List and Product Details

---
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using MecaAgenda.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MecaAgenda.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IServiceProduct _serviceProduct;
+
+        public ProductController(IServiceProduct serviceProduct)
+        {
+            _serviceProduct = serviceProduct;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var collection = await _serviceProduct.ListAsync();
+            return View(collection);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                var @object = await _serviceProduct.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Product does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
