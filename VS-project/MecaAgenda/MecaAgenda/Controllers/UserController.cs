cc62f1f0467a96c8343bfa0aeb466c120ce90732
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-06-18 19:14:24 -0600

    Add User List Interface

---
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using MecaAgenda.Application.Services.Implementations;
+using MecaAgenda.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MecaAgenda.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IServiceUser _serviceUser;
+
+        public UserController(IServiceUser serviceUser)
+        {
+            _serviceUser = serviceUser;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var collection = await _serviceUser.ListAsync();
+            return View(collection);
+        }
+    }
+}
