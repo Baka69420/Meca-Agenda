724e3fe46457ba7465d7be4421ff99935bc8468d
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-07-16 16:08:34 -0600

    Add Procuct in FrontEnd
    Add options to list, see details, create, edit and delete Services on the website

--- VS-project/MecaAgenda/MecaAgenda/Controllers/ProductController.cs
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using MecaAgenda.Application.Services.Interfaces;
+﻿using MecaAgenda.Application.DTOs;
+using MecaAgenda.Application.Services.Implementations;
+using MecaAgenda.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MecaAgenda.Web.Controllers
@@ -6,10 +8,12 @@ namespace MecaAgenda.Web.Controllers
     public class ProductController : Controller
     {
         private readonly IServiceProduct _serviceProduct;
+        private readonly IServiceCategory _serviceCategory;
 
-        public ProductController(IServiceProduct serviceProduct)
+        public ProductController(IServiceProduct serviceProduct, IServiceCategory serviceCategory)
         {
             _serviceProduct = serviceProduct;
+            _serviceCategory = serviceCategory;
         }
 
         [HttpGet]
@@ -42,6 +46,137 @@ namespace MecaAgenda.Web.Controllers
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAdmin()
+        {
+            var collection = await _serviceProduct.ListAsync();
+            return View(collection);
+        }
+        [HttpGet]
+        public async Task<ActionResult> CreateAsync()
+        {
+            ViewBag.ListCategory = await _serviceCategory.ListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductDTO productDTO)
+        {
+            productDTO.Category = await _serviceCategory.GetAsync(productDTO.CategoryId);
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceProduct.AddAsync(productDTO);
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            ViewBag.ListCategory = await _serviceCategory.ListAsync();
+
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceProduct.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Product does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, ProductDTO productDTO)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            productDTO.ProductId = id.Value;
+
+            await _serviceProduct.UpdateAsync(productDTO);
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceProduct.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Product does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id, IFormCollection collection)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceProduct.DeleteAsync(id.Value);
+            return RedirectToAction("IndexAdmin");
         }
     }
 }
