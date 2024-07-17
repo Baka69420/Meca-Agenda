287300d10dc094dbfe4749367a383d8b6d4698c8
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-07-16 18:28:35 -0600

    Add Procuct in FrontEnd
    Add options to list, see details, create, edit and delete Services on the website

---
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/BranchController.cs
@@ -0,0 +1,168 @@
+﻿using MecaAgenda.Application.DTOs;
+using MecaAgenda.Application.Services.Implementations;
+using MecaAgenda.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MecaAgenda.Web.Controllers
+{
+    public class BranchController : Controller
+    {
+        private readonly IServiceBranch _serviceBranch;
+
+        public BranchController(IServiceBranch serviceBranch)
+        {
+            _serviceBranch = serviceBranch;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAdmin()
+        {
+            var collection = await _serviceBranch.ListAsync();
+            return View(collection);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranch.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Branch does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(BranchDTO branchDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceBranch.AddAsync(branchDTO);
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranch.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Service does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, BranchDTO branchDTO)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            branchDTO.BranchId = id.Value;
+
+            await _serviceBranch.UpdateAsync(branchDTO);
+            return RedirectToAction("IndexAdmin");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranch.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Branch does not exist");
+                }
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id, IFormCollection collection)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceBranch.DeleteAsync(id.Value);
+            return RedirectToAction("IndexAdmin");
+        }
+    }
+}
