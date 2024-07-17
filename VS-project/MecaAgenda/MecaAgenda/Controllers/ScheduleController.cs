aa216143429949314f41d950786a3dd845d7c946
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-07-17 12:28:02 -0600

    Add Schedule in FrontEnd
    Add options to list, see details

---
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/ScheduleController.cs
@@ -0,0 +1,47 @@
+﻿using MecaAgenda.Application.DTOs;
+using MecaAgenda.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MecaAgenda.Web.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private readonly IServiceBranch _serviceBranch;
+        private readonly IServiceBranchSchedule _serviceBranchSchedule;
+        private readonly IServiceScheduleException _serviceScheduleException;
+
+        public ScheduleController(IServiceBranch serviceBranch, IServiceBranchSchedule serviceBranchSchedule, IServiceScheduleException serviceScheduleException)
+        {
+            _serviceBranch = serviceBranch;
+            _serviceBranchSchedule = serviceBranchSchedule;
+            _serviceScheduleException = serviceScheduleException;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAdmin()
+        {
+            var collection = await _serviceBranch.ListAsync();
+            return View(collection);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranch.GetAsync(id.Value);
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
