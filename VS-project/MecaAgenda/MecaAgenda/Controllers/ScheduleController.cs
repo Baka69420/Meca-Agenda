ba0cbff7c7e903973ff6c2bb0196b957795ec538
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-07-19 09:43:31 -0600

    Changes to Schedule in frontend
    - Add options to show create, edit and delete options on the schedule and exceptions
    - Add the Create Schedule Menu

--- VS-project/MecaAgenda/MecaAgenda/Controllers/ScheduleController.cs
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/ScheduleController.cs
@@ -1,4 +1,5 @@
 ﻿using MecaAgenda.Application.DTOs;
+using MecaAgenda.Application.Services.Implementations;
 using MecaAgenda.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -42,6 +43,129 @@ namespace MecaAgenda.Web.Controllers
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> CreateSchedule(int? branchId)
+        {
+            if (branchId == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            ViewBag.Branch = await _serviceBranch.GetAsync(branchId.Value);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateSchedule(BranchScheduleDTO branchScheduleDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceBranchSchedule.AddAsync(branchScheduleDTO);
+            return RedirectToAction("Details", new { id = branchScheduleDTO.BranchId });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EditSchedule(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranchSchedule.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Product does not exist");
+                }
+
+                ViewBag.Branch = await _serviceBranch.GetAsync(@object.BranchId);
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditSchedule(BranchScheduleDTO branchScheduleDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceBranchSchedule.UpdateAsync(branchScheduleDTO);
+            return RedirectToAction("Details", new { id = branchScheduleDTO.BranchId });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DeleteSchedule(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("IndexAdmin");
+                }
+
+                var @object = await _serviceBranchSchedule.GetAsync(id.Value);
+
+                if (@object == null)
+                {
+                    throw new Exception("Product does not exist");
+                }
+
+                ViewBag.Branch = await _serviceBranch.GetAsync(@object.BranchId);
+
+                return View(@object);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteSchedule(int? id, int? branchId, IFormCollection collection)
+        {
+            if (id == null || branchId == null)
+            {
+                return RedirectToAction("IndexAdmin");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("; ", ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage));
+                ViewBag.ErrorMessage = errors;
+                return View();
+            }
+
+            await _serviceBranchSchedule.DeleteAsync(id.Value);
+            return RedirectToAction("Details", new { id = branchId });
         }
     }
 }
