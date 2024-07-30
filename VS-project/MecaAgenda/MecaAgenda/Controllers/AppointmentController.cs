448c4848812fad8ef6fc0bd76ee7f3dfe0603a9c
Rafael Alvarado <74164320+Baka69420@users.noreply.github.com>
2024-07-30 15:17:00 -0600

    Filter Appointment
    Add function to filter appointment by different values

--- VS-project/MecaAgenda/MecaAgenda/Controllers/AppointmentController.cs
+++ VS-project/MecaAgenda/MecaAgenda/Controllers/AppointmentController.cs
@@ -1,23 +1,62 @@
 ﻿using MecaAgenda.Application.Services.Implementations;
 using MecaAgenda.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace MecaAgenda.Web.Controllers
 {
     public class AppointmentController : Controller
     {
         private readonly IServiceAppointment _serviceAppointment;
+        private readonly IServiceBranch _serviceBranch;
+        private readonly IServiceUser _serviceUser;
 
-        public AppointmentController(IServiceAppointment serviceAppointment)
+        public AppointmentController(IServiceAppointment serviceAppointment, IServiceBranch serviceBranch, IServiceUser serviceUser)
         {
             _serviceAppointment = serviceAppointment;
+            _serviceBranch = serviceBranch;
+            _serviceUser = serviceUser;
         }
 
+        [HttpGet]
         public async Task<IActionResult> IndexAdmin()
         {
-            var collection = await _serviceAppointment.ListAsync(null, null, null);
+            var branches = await _serviceBranch.ListAsync("");
+            var clients = await _serviceUser.ListAsync("client", "");
 
-            return View(collection);
+            var branchOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "", Text = "--- Select a Branch ---" }
+            };
+            var clientOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "", Text = "--- Select a Client ---" }
+            };
+
+            branchOptions.AddRange(branches.Select(item => new SelectListItem
+            {
+                Value = item.BranchId.ToString(),
+                Text = item.Name
+            }));
+            clientOptions.AddRange(clients.Select(item => new SelectListItem
+            {
+                Value = item.UserId.ToString(),
+                Text = item.Name
+            }));
+
+            ViewBag.Branches = branchOptions;
+            ViewBag.Clients = clientOptions;
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointments(int? idBranch, int? idClient, DateOnly? appointmentStartDate, DateOnly? appointmentEndDate)
+        {
+            var collection = await _serviceAppointment.ListAsync(idBranch, idClient, null);
+
+            return PartialView("_AppointmentTableAdmin", collection);
         }
 
         [HttpGet]
